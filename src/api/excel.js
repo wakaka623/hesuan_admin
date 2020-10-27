4cdb909cabbfca0dc8fae1643625b7e69fb6161d
fan <5682715+fan_01@user.noreply.gitee.com>
2020-10-27 17:48:24 +0800

    feat: 新增excel表格导入，页面生命周期展示表格数据

--- src/api/excel.js
+++ src/api/excel.js
@@ -1,12 +1,36 @@
-import request from '@/utils/request'
+// import request from '@/utils/request'
+import axios from 'axios';
 
 export function importExcel(file) {
+  const data = file;
+
+  return axios({
+    url: '/api/import/excel',
+    method: 'post',
+    headers: {'Content-Type': 'multpart/form-data'},
+    data
+  });
+}
+
+export function getTableHeader(tableName) {
   const data = {
-    excel: file
+    table_name: tableName
   };
 
-  return request({
-    url: '/api/import/excel',
+  return axios({
+    url: '/api/import/excel/columns',
+    method: 'post',
+    data
+  });
+}
+
+export function getTableDatas(tableName) {
+  const data = {
+    table_name: tableName
+  };
+
+  return axios({
+    url: '/api/import/excel/get_table_data',
     method: 'post',
     data
   });
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -10,53 +10,38 @@
       </el-row>
     </div>
     <div class="main">
-      <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: auto" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="date" label="日期" width="120"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-        <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+        <el-table-column v-for="(item, index) in tableHeadData" :key="index" :prop="item.COLUMN_NAME" :label="item.column_comment"></el-table-column>
+        <!-- <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column> -->
       </el-table>
     </div>
   </div>
 </template>
 
 <script>
-import { importExcel } from '@/api/excel';
+import { getTableHeader, getTableDatas, importExcel } from '@/api/excel';
 import axios from 'axios';
 
 export default {
   name: 'test',
   data() {
     return {
-      tableData: [{
+      // 表格标题
+      tableHeadData: [
+        {
+          comment: '',
+          value: ''
+        }
+      ],
+      tableData: [
+        {
           date: '2016-05-03',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-08',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-06',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-      }],
+        }
+      ],
       multipleSelection: []
     }
   },
@@ -71,43 +56,48 @@ export default {
      * 上传文件
      */
     handleFileChange(e) {
-      // console.log(this.files);
-      console.log(e);
       const files = e.target.files[0];
       var formData = new FormData();
 
-      formData.append('username', 'Chris');
-      console.log(formData);
-      console.log(formData.entries());
-
-      formData.append('key1', 'value1');
-      formData.append('excel', files);
-      formData.append('key2', 'value2');
+      formData.append('file', files);
 
 
-      console.log('上传文件');
-      console.log(formData.getAll());
+      console.log('开始上传文件');
 
-      axios({
-        url: '/api/import/excel',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        method: 'post',
-        data: {
-          formData
-        }
-      })
-
-      return;
       importExcel(formData)
         .then(res => {
-          console.log(res);
+          console.log(res.data);
+
+          return;
+          this.tableHeadData = res.data.splice(0, 1)[0];
+
+          this.tableData = res.data;
+
         });
     },
 
     handleSelectionChange(val) {
       this.multipleSelection = val;
     }
-  }
+  },
+
+  mounted() {
+    // 获取表格标题栏
+    getTableHeader('ruida_fund_reconciliation')
+      .then(res => {
+        console.log(res);
+
+        this.tableHeadData = res.data;
+      });
+
+    // 获取表格数据
+    getTableDatas('ruida_fund_reconciliation')
+      .then(res => {
+        console.log(res);
+
+        this.tableData = res.data;
+      });
+  },
 }
 </script>
 
@@ -115,5 +105,10 @@ export default {
 #excel-upload-input{
   display: none;
   z-index: -9999;
+}
+
+.main {
+  max-height: 500px;
+  overflow-y: auto;
 }
 </style>
